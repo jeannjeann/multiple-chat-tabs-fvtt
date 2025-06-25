bf386df5bcec43e8bd20668524112a2df146607e
jeannjeann <136861083+jeannjeann@users.noreplay.github.com>
2025-06-25 20:50:43 +0900

    add/remove tabs, bottom tabbar

--- module/mct.js
+++ module/mct.js
@@ -1,12 +1,32 @@
+// MultipleChatTabs Class
 class MultipleChatTabs {
+  // Helper to get configured tabs
+  static getTabs() {
+    const tabString = game.settings.get("multiple-chat-tabs", "tabs") || "Tab1";
+    return tabString
+      .split(",")
+      .map((label, index) => ({
+        id: `tab-${index}`,
+        label: label.trim(),
+      }))
+      .filter((tab) => tab.label);
+  }
+
   // Set default tab
-  static activeFilter = "tab1";
+  static activeFilter = "tab-0";
 
   /**
    * Add tabs UI
    * @param {jQuery} html
    */
   static async addTabs(html) {
+    const tabs = this.getTabs();
+    if (tabs.length === 0) return;
+
+    if (!tabs.some((t) => t.id === this.activeFilter)) {
+      this.activeFilter = tabs[0]?.id || "tab-0";
+    }
+
     const existingTabs = html.find(".multiple-chat-tabs-nav");
     if (existingTabs.length > 0) {
       existingTabs.find(".item").removeClass("active");
@@ -18,7 +38,7 @@ class MultipleChatTabs {
 
     const tabsHtml = await renderTemplate(
       "modules/multiple-chat-tabs/templates/chat-tabs.hbs",
-      {}
+      { tabs: tabs }
     );
     const tabsElement = $(tabsHtml);
 
@@ -27,8 +47,7 @@ class MultipleChatTabs {
       .find(`.item[data-filter="${this.activeFilter}"]`)
       .addClass("active");
 
-    tabsElement.find(".item").on("click", this._onTabClick.bind(this));
-    html.find("#chat-log").before(tabsElement);
+    html.find("#chat-log").after(tabsElement);
   }
 
   /**
@@ -39,15 +58,16 @@ class MultipleChatTabs {
     event.preventDefault();
     const clickedFilter = event.currentTarget.dataset.filter;
 
-    if (this.activeFilter === clickedFilter) return;
+    if (MultipleChatTabs.activeFilter === clickedFilter) return;
 
-    this.activeFilter = clickedFilter;
+    MultipleChatTabs.activeFilter = clickedFilter;
 
     const nav = $(event.currentTarget).closest(".multiple-chat-tabs-nav");
     nav.find(".item").removeClass("active");
     $(event.currentTarget).addClass("active");
 
-    this.applyFilter();
+    const appElement = $(event.currentTarget).closest(".app");
+    MultipleChatTabs.applyFilter(appElement);
   }
 
   /**
@@ -57,6 +77,10 @@ class MultipleChatTabs {
   static applyFilter(scope) {
     const chatLog = scope ? scope.find("#chat-log") : $("#chat-log");
     if (!chatLog.length) return;
+
+    // Get tabID
+    const tabs = this.getTabs();
+    const validTabIds = new Set(tabs.map((t) => t.id));
 
     const messages = chatLog.find(".message");
 
@@ -72,10 +96,11 @@ class MultipleChatTabs {
       let show = false;
       const sourceTab = message.getFlag("multiple-chat-tabs", "sourceTab");
 
-      if (sourceTab) {
+      // Check tab existing
+      if (sourceTab && validTabIds.has(sourceTab)) {
         show = sourceTab === this.activeFilter;
       } else {
-        show = this.activeFilter === "tab1";
+        show = this.activeFilter === "tab-0";
       }
 
       messageElement.toggle(show);
@@ -87,13 +112,61 @@ class MultipleChatTabs {
   }
 }
 
+// TabSettings Class
+class TabSettings extends FormApplication {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      title: game.i18n.localize("MCT.Settings.WindowTitle"),
+      id: "multiple-chat-tabs-settings",
+      template: "modules/multiple-chat-tabs/templates/tab-settings.hbs",
+      width: 400,
+      height: "auto",
+      resizable: true,
+    });
+  }
+
+  getData(options) {
+    const data = super.getData(options);
+    data.tabs = game.settings.get("multiple-chat-tabs", "tabs");
+    return data;
+  }
+
+  async _updateObject(event, formData) {
+    await game.settings.set("multiple-chat-tabs", "tabs", formData.tabs);
+  }
+}
+
 /**
  * Initialize module
  */
 Hooks.once("init", async function () {
-  const templatePaths = ["modules/multiple-chat-tabs/templates/chat-tabs.hbs"];
+  // Load templates
+  const templatePaths = [
+    "modules/multiple-chat-tabs/templates/chat-tabs.hbs",
+    "modules/multiple-chat-tabs/templates/tab-settings.hbs",
+  ];
   await loadTemplates(templatePaths);
-  console.log("Chat Filter Tabs | Templates pre-loaded.");
+
+  // Configure
+  game.settings.registerMenu("multiple-chat-tabs", "tab-settings", {
+    name: "MCT.Settings.Menu.Name",
+    label: "MCT.Settings.Menu.Label",
+    hint: "MCT.Settings.Menu.Hint",
+    icon: "fas fa-tasks",
+    type: TabSettings,
+    restricted: true,
+  });
+  game.settings.register("multiple-chat-tabs", "tabs", {
+    scope: "world",
+    config: false,
+    type: String,
+    default: "Tab1,Tab2,Tab3,Tab4",
+    onChange: () => {
+      if (ui.chat) {
+        ui.chat.render();
+      }
+    },
+  });
 
   // Override ChatLog.scrollBottom method
   ChatLog.prototype.scrollBottom = function () {
@@ -109,6 +182,14 @@ Hooks.once("init", async function () {
   Hooks.on("renderChatLog", async (app, html, data) => {
     await MultipleChatTabs.addTabs(html);
     MultipleChatTabs.applyFilter(html);
+
+    // Click listener
+    html.off("click", ".multiple-chat-tabs-nav .item");
+    html.on(
+      "click",
+      ".multiple-chat-tabs-nav .item",
+      MultipleChatTabs._onTabClick
+    );
   });
 
   Hooks.on("preCreateChatMessage", (message, data, options, userId) => {
